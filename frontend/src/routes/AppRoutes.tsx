e6a99a511aa632eef4fd735a47dda051dde1256d
fgutierrezalbornoz <francisco.gutial@gmail.com>
2025-09-12 16:37:44 -0300

    add: Routing

---
+++ frontend/src/routes/AppRoutes.tsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router";
+export function AppRoutes(){
+  return (
+    <Routes>
+        <Route
+            index
+            element={<h1>Home</h1>}
+        />
+        <Route
+            path='/products/:id'
+            element={<h1>Página de un producto</h1>}
+        />
+        <Route
+            path='*'
+            element={<h1>Not Found 404</h1>}
+        />
+    </Routes>
+  );
+};
