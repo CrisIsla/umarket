1618bbf2f7b0e4cd29cbc82776be1d860b80e814
fgutierrezalbornoz <francisco.gutial@gmail.com>
2025-09-12 17:02:31 -0300

    modify: title in index.html. add: routing with alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import packageJson from './package.json'
+import { createHtmlPlugin } from 'vite-plugin-html';
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          title: packageJson.productName,
+        },
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
