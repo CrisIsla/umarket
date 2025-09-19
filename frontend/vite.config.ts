aa03450aade10cb83b9bb17c520cc43613f80788
Sebastian Salas Lavado <sebastian.salas.l@ug.uchile.cl>
2025-09-19 02:48:44 -0300

    deps(front): added tailwindcss to plugins

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,11 +3,13 @@ import react from '@vitejs/plugin-react'
 import packageJson from './package.json'
 import { createHtmlPlugin } from 'vite-plugin-html';
 import path from 'path'
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     createHtmlPlugin({
       inject: {
         data: {
