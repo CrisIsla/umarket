f2812c02cf7230a12bbb28e5006eb728e19f2d03
Sebastian Salas Lavado <sebastian.salas.l@ug.uchile.cl>
2025-09-19 02:46:33 -0300

    style(front): changed code format using Prettier

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,11 +1,11 @@
-import { ProductForm } from "@/components/ProductForm";
+import { NewProductPage } from "@/pages/NewProductPage";
 import { Route, Routes } from "react-router";
 export function AppRoutes() {
   return (
     <Routes>
       <Route index element={<h1>Home</h1>} />
       <Route path="/products/:id" element={<h1>Página de un producto</h1>} />
-      <Route path="/products/new" element={<ProductForm />} />
+      <Route path="/products/new" element={<NewProductPage />} />
       <Route path="*" element={<h1>Not Found 404</h1>} />
     </Routes>
   );
