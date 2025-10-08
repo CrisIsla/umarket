ba61108837611fd0f36b61117f7526bab50791a4
Cristobal <cristobal.isla@ug.uchile.cl>
2025-10-08 10:01:02 -0300

    feat: add product schema

---
+++ backend/controllers/products.ts
@@ -0,0 +1,69 @@
+import mongoose from "mongoose";
+
+mongoose.set("strictQuery", false);
+
+interface ProductData {
+  readonly id: string;
+  title: string;
+  date: string | Date;
+  description: string;
+  seller: mongoose.Types.ObjectId;
+  photos: string[] | [];
+  condition: "new" | "used";
+  price: number;
+  category: string;
+  tags: string[];
+}
+
+const productSchema = new mongoose.Schema<ProductData>({
+  title: String,
+  date: {
+    type: Date,
+    default: () => Date.now(),
+  },
+  description: {
+    type: String,
+    minLength: 5,
+    maxLength: 2048,
+    required: true,
+  },
+  seller: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  photos: {
+    type: [String],
+    minLength: 0,
+    maxLength: 5,
+  },
+  condition: {
+    type: String,
+    validate: {
+      validator: (v) => v === "new" || v === "used",
+      message: (props) =>
+        `Condition must be 'used' or 'new', but got '${props.value}' instead.`,
+    },
+  },
+  price: { type: Number, min: 0 },
+  category: String,
+  tags: { type: [String], maxLength: 5 },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+productSchema.set("toJSON", {
+  transform: (
+    document,
+    returnedObject: {
+      id?: string;
+      _id?: mongoose.Types.ObjectId;
+      __v?: number;
+    },
+  ) => {
+    returnedObject.id = returnedObject._id?.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+export default Product;
