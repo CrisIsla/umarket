c77de7c97c23d46d4429cd37668504aaf2f8937b
fgutierrezalbornoz <francisco.gutial@gmail.com>
2025-10-15 14:52:28 -0300

    feat (frontend): images added in product form are sent to backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,11 +3,12 @@ import express from "express";
 import { authErrorHandler } from "./utils/authMiddleware";
 import errorHandler from "./utils/errorMiddleware";
 import productRouter from "./controllers/product";
+import cors from "cors";
 const app = express();
 
 app.use(express.static("dist"));
 app.use(express.json());
-
+app.use(cors());
 app.use(cookieParser());
 
 app.use("/products", productRouter);
