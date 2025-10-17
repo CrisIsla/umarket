bcdb29fa92bbc4bf2a286da302f70e54301e5d1b
Sebastián Salas Lavado <sebastian.salas.l@ug.uchile.cl>
2025-10-17 06:24:51 -0300

    feat: added auth routes

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,7 +1,9 @@
 import { NewProductPage } from "@/pages/NewProductPage";
 import { ProductDetailPage } from "@/pages/ProductDetailPage";
 import { Route, Routes } from "react-router";
+import { LoginPage } from "@/pages/LoginPage.tsx";
 import Home from "../views/Home.tsx";
+import { RegisterPage } from "@/pages/RegisterPage.tsx";
 
 export function AppRoutes() {
   return (
@@ -9,6 +11,9 @@ export function AppRoutes() {
       <Route index element={<Home />} />
       <Route path="/products/new" element={<NewProductPage />} />
       <Route path="/products/:id" element={<ProductDetailPage />} />
+      <Route path="/login" element={<LoginPage/>} />
+      <Route path="/register" element={<RegisterPage/>} />
+
       <Route path="*" element={<h1>Not Found 404</h1>} />
     </Routes>
   );
