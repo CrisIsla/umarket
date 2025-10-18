8f8307a5c6152b04cb01eb4d62e75c547ae91521
Josefina <josefina.labarca@ug.uchile.cl>
2025-10-18 11:21:34 -0300

    feat: add /auth route with authController

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,13 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import { authErrorHandler } from "./utils/authMiddleware";
+import authController from "./controllers/authController";
 const app = express();
 
 app.use(express.static("dist"));
 app.use(express.json());
 
 app.use(cookieParser());
-
+app.use("/auth", authController);
 app.use(authErrorHandler);
 export default app;
