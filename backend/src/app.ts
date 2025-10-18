392a43a94417d9f54fc4bc1ffbed173b55feb176
Cristobal <cristobal.isla@ug.uchile.cl>
2025-10-18 16:21:54 -0300

    add error handler middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import { authErrorHandler } from "./utils/authMiddleware";
+import errorHandler from "./utils/errorMiddleware";
 const app = express();
 
 app.use(express.static("dist"));
@@ -9,4 +10,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(authErrorHandler);
+app.use(errorHandler);
 export default app;
+
