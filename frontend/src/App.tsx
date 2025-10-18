deb93629d61d8775921c7ee2cdc96f6e8a171b95
Josefina <josefina.labarca@ug.uchile.cl>
2025-10-18 18:50:21 -0300

    fix: make search bar work across SPA

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import { AppRoutes } from "./routes/AppRoutes";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <div className="mt-17">
         <AppRoutes />
       </div>
