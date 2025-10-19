3fc820a83b828f587908f3668383a862397a0e2d
Sebastián Salas Lavado <sebastian.salas.l@ug.uchile.cl>
2025-10-19 04:14:32 -0300

    fix: link on header to navigate to the correct route

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -50,7 +50,7 @@ export default function Header({ onSearch }: HeaderProps) {
 
       {/* Actions */}
       <nav className="flex items-center space-x-12 px-6 py-2">
-        <Link to="/products/new" className="text-white">Vender</Link>
+        <Link to="/new/products" className="text-white">Vender</Link>
         <Link to="/login" className="text-white">Iniciar sesión</Link>
         <Link to="/carrito" className="text-white flex items-center space-x-1">
           <ShoppingCart className="h-5 w-5" />
