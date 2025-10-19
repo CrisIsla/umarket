5a46c6ab67d55a7be67b430ddaca9b059e3444d8
fgutierrezalbornoz <francisco.gutial@gmail.com>
2025-10-19 11:28:52 -0300

    feat (backend): add db connection

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,20 @@ import { authErrorHandler } from "./utils/authMiddleware";
 import errorHandler from "./utils/errorMiddleware";
 import authController from "./controllers/authController";
 import productRouter from "./controllers/product";
+import mongoose from "mongoose";
+import config from "./utils/config";
+import logger from "./utils/logger";
+
+const { MONGODB_DBNAME, MONGODB_URI } = config;
 const app = express();
+
+mongoose.set("strictQuery", false);
+
+if (MONGODB_URI) {
+  mongoose.connect(MONGODB_URI, { dbName: MONGODB_DBNAME }).catch((error) => {
+    logger.error("error connecting to MongoDB:", error.message);
+  });
+}
 
 app.use(express.static("dist"));
 app.use(express.json());
