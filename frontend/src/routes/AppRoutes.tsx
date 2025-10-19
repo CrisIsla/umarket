ab7b5886b573034d66e99acfffe94a57dada8466
Sebastián Salas Lavado <sebastian.salas.l@ug.uchile.cl>
2025-10-19 01:32:39 -0300

    feat: used protected and public routes handling

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,20 +1,26 @@
-import { NewProductPage } from "@/pages/NewProductPage";
-import { ProductDetailPage } from "@/pages/ProductDetailPage";
 import { Route, Routes } from "react-router";
-import { LoginPage } from "@/pages/LoginPage.tsx";
-import Home from "../views/Home.tsx";
-import { RegisterPage } from "@/pages/RegisterPage.tsx";
+import { useCheckAuth } from "@/hooks/useCheckAuth";
+import { ProtectedRoutes } from "@/components/ProtectedRoutes";
+import { PublicRoutes } from "@/components/PublicRoutes";
+import { ProductDetailPage } from "@/pages/ProductDetailPage";
+import { CheckingAuth } from "@/components/CheckingAuth";
+import Home from "@/views/Home";
 
-export function AppRoutes() {
+export const AppRoutes = () => {
+  const { status } = useCheckAuth();
+
+  if (status === "checking") return <CheckingAuth />;
+
   return (
     <Routes>
+      {status === "authenticated" ? (
+        <Route path="/*" element={<ProtectedRoutes />} />
+      ) : (
+        <Route path="/auth/*" element={<PublicRoutes />} />
+      )}
       <Route index element={<Home />} />
-      <Route path="/products/new" element={<NewProductPage />} />
       <Route path="/products/:id" element={<ProductDetailPage />} />
-      <Route path="/login" element={<LoginPage/>} />
-      <Route path="/register" element={<RegisterPage/>} />
-
       <Route path="*" element={<h1>Not Found 404</h1>} />
     </Routes>
   );
-}
+};
