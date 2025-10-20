8f675c6c5d4bdaa493c9ada181561dd48b2ab8be
fgutierrezalbornoz <francisco.gutial@gmail.com>
2025-10-20 10:45:41 -0300

    refactor (backend): Modify middlewares

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,14 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import { authErrorHandler } from "./utils/authMiddleware";
-import errorHandler from "./utils/errorMiddleware";
 import authController from "./controllers/authController";
 import productRouter from "./controllers/product";
 import mongoose from "mongoose";
 import config from "./utils/config";
 import logger from "./utils/logger";
-
+import middleware from "./utils/middleware";
 const { MONGODB_DBNAME, MONGODB_URI } = config;
+const { requestLogger, errorHandler, unknownEndpoint } = middleware
 const app = express();
 
 mongoose.set("strictQuery", false);
@@ -18,15 +18,16 @@ if (MONGODB_URI) {
     logger.error("error connecting to MongoDB:", error.message);
   });
 }
-
 app.use(express.static("dist"));
 app.use(express.json());
+app.use(requestLogger)
 
 app.use(cookieParser());
 
-app.use("/products", productRouter);
-app.use("/auth", authController);
+app.use("/api/products", productRouter);
+app.use("/api/auth", authController);
 
 app.use(authErrorHandler);
+app.use(unknownEndpoint);
 app.use(errorHandler);
 export default app;
