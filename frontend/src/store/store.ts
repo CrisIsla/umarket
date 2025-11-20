9e6c6d88743760044ee558c74a38a5449679734d
Sebastián Salas Lavado <sebastian.salas.l@ug.uchile.cl>
2025-11-20 04:15:35 -0300

    fix(core): added remaining types to define appthunk type

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -9,3 +9,6 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+import type { ThunkAction } from 'redux-thunk'
+import type { Action } from 'redux'
+export type AppThunk<ReturnType = Promise<void>> = ThunkAction<ReturnType, RootState, unknown, Action<string>>
