8a6a41139add5b0f234de00116a5ddf3727135ee
Sebastián Salas Lavado <sebastian.salas.l@ug.uchile.cl>
2025-11-20 20:42:22 -0300

    refactor(core): replaced anchor tag with link for logo

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,12 +1,13 @@
 import { LoginForm } from "@/components/LoginForm";
 import logo from "../assets/logo.svg";
+import { Link } from "react-router";
 
 export const LoginPage = () => {
   return (
     <div className="flex flex-col items-center p-5 mt-8">
-      <a href="/">
-        <img className="w-44 h-auto" src={logo} alt="U-Market" />
-      </a>
+      <Link to="/" className="flex items-center px-4 py-1">
+        <img src={logo} alt="Umarket Logo" className="w-44 h-auto" />
+      </Link>
       <LoginForm />
     </div>
   );
