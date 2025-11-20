a9c95f21286ee26d78faf55fdabf2e6ab918d414
Sebastián Salas Lavado <sebastian.salas.l@ug.uchile.cl>
2025-11-20 04:08:01 -0300

    refactor(frontend): simplified LoginPage, removed prop drilling

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,18 +1,13 @@
 import { LoginForm } from "@/components/LoginForm";
 import logo from "../assets/logo.svg";
-import type { Dispatch, SetStateAction } from "react";
 
-interface Props {
-  setCsrfToken : Dispatch<SetStateAction<string | null>>
-}
-
-export const LoginPage = ({setCsrfToken}: Props) => {
+export const LoginPage = () => {
   return (
     <div className="flex flex-col items-center p-5 mt-8">
       <a href="/">
         <img className="w-44 h-auto" src={logo} alt="U-Market" />
       </a>
-      <LoginForm setCsrfToken={setCsrfToken}/>
+      <LoginForm />
     </div>
   );
 };
